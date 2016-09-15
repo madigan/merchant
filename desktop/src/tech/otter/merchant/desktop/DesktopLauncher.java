acda6eab7e527001b8769499ac9134138cd50a3d
John Lynn <john@silferein.com>
2016-09-14 18:31:19 -0700

    Initialize repository.

---
+++ desktop/src/tech/otter/merchant/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tech.otter.merchant.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tech.otter.merchant.MerchantGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MerchantGame(), config);
+	}
+}
---
+++ html/src/tech/otter/merchant/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package tech.otter.merchant.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tech.otter.merchant.MerchantGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MerchantGame();
+        }
+}
